946ba75f0324784a373473e2ec77a82f70d95a12
tengrommel <wwwsousoucom@126.com>
2017-12-28 21:55:32 +0800

    重命名

--- ch01/package_example/main/main.go
+++ ch01/package_example/main/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/tengrommel/go_jobs/ch01/package_example/calc"
+	a "github.com/tengrommel/go_jobs/ch01/package_example/calc"
 )
 
 func main() {
-	sum := calc.Add(100, 300)
-	sub := calc.Sub(100, 300)
+	sum := a.Add(100, 300)
+	sub := a.Sub(100, 300)
 	fmt.Println("sum=", sum)
 	fmt.Println("sub=", sub)
 }
