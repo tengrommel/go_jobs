8cd7f0fc015914c9354a35c5f8017a2946261e07
tengrommel <wwwsousoucom@126.com>
2017-12-29 22:57:57 +0800

    多返回值的应用

--- ch02/example9/main/main.go
+++ ch02/example9/main/main.go
@@ -4,17 +4,24 @@ import (
 	"fmt"
 )
 
-func swap(a int, b int) {
-	tmp := a
-	a = b
-	b = tmp
-	return
+func swap(a *int, b *int) {
+	tmp := *a
+	*a = *b
+	*b = tmp
+}
+
+func swap1(a int, b int) (int, int) {
+	return b, a
 }
 
 func main() {
 	first := 100
 	second := 200
-	swap(first, second)
+	swap(&first, &second)
+	fmt.Println("first=", first)
+	fmt.Println("second=", second)
+
+	swap1(first, second)
 	fmt.Println("first=", first)
 	fmt.Println("second=", second)
 }
