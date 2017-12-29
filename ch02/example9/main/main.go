8866c60cef0e7c6acc0886183d1ff0b6e0a66cc4
tengrommel <wwwsousoucom@126.com>
2017-12-29 22:45:04 +0800

    值拷贝

---
+++ ch02/example9/main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func swap(a int, b int) {
+	tmp := a
+	a = b
+	b = tmp
+	return
+}
+
+func main() {
+	first := 100
+	second := 200
+	swap(first, second)
+	fmt.Println("first=", first)
+	fmt.Println("second=", second)
+}
