c921107057cd6e7e1f5d4f5d0713a2f7f6880dff
tengrommel <wwwsousoucom@126.com>
2017-12-29 22:22:42 +0800

    &a

--- ch02/example8/main/main.go
+++ ch02/example8/main/main.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 )
 
+func modify(a int) {
+	a = 10
+}
+
+func modify1(a *int) {
+	*a = 10
+}
+
 func main() {
 	a := 5
 	b := make(chan int, 1)
 	fmt.Println("a=", a)
 	fmt.Println("b=", b)
+	modify(a)
+	fmt.Println("a=", a)
+	modify1(&a)
+	fmt.Println("a=", a)
 }
