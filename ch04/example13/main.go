99c1af732c17e38b58c8d28ca2f1e9dc1a860842
tengrommel <wwwsousoucom@126.com>
2017-12-30 16:24:11 +0800

    原子操作和读写锁

--- ch04/example13/main.go
+++ ch04/example13/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"sync"
 	"time"
+	"sync/atomic"
 )
 
 var lock sync.Mutex
+var rwlock sync.RWMutex
 
 func testMap()  {
 	var a map[int]int
@@ -31,8 +33,37 @@ func testMap()  {
 	time.Sleep(time.Second)
 }
 
+func testRWLock()  {
+	var a map[int]int
+	a = make(map[int]int, 5)
+	var count int64
+	a[2]=1
+	a[8]=8
+	a[1]=5
+	a[4]=4
+	a[5]=1
+	for i := 0; i<2;i++{
+		go func(b map[int]int) {
+			rwlock.Lock()
+			b[9] = rand.Intn(100)
+			rwlock.Unlock()
+		}(a)
+	}
+	for i := 0; i<100;i++{
+		go func(b map[int]int) {
+			rwlock.RLock()
+			fmt.Println(a)
+			rwlock.RUnlock()
+			atomic.AddInt64(&count,1)
+		}(a)
+	}
+
+	time.Sleep(10*time.Second)
+	fmt.Println(atomic.LoadInt64(&count))
+}
+
 func main() {
 	testMap()
-
+	testRWLock()
 
 }
