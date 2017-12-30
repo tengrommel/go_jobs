13f613fbb50dbcc058715ee374a5a34842e5eb0d
tengrommel <wwwsousoucom@126.com>
2017-12-30 10:23:26 +0800

    strings

---
+++ ch03/example/example4/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "time"
+import "fmt"
+
+func test() {
+	time.Sleep(time.Millisecond * 100)
+}
+
+func main() {
+	now := time.Now()
+	fmt.Println(now.Format("2006/01/02 15:04:05"))
+
+	start := time.Now().UnixNano()
+	test()
+	end := time.Now().UnixNano()
+	fmt.Printf("cost:%d us\n", (end-start)/1000)
+}
