36e6c73b59df9707409d2d0d37924be5ba88a56b
tengrommel <wwwsousoucom@126.com>
2017-12-30 22:43:04 +0800

    ZHI

---
+++ ch05/example/example9/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type Test interface {
+	Print()
+}
+
+type Student struct {
+	name string
+	age int
+	score int
+}
+
+func (p Student)Print()  {
+	fmt.Println("name:", p.name)
+	fmt.Println("age:", p.age)
+	fmt.Println("score:", p.score)
+}
+
+func main() {
+	var t Test
+	var stu Student = Student{
+		name: "stu1",
+		age:20,
+		score:89,
+	}
+	t = stu
+	t.Print()
+}
