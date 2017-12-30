6357b4c833b91f406cbffe909e07a9bbacc6aa21
tengrommel <wwwsousoucom@126.com>
2017-12-31 01:29:53 +0800

    .Type

---
+++ ch06/example3/main.go
@@ -0,0 +1,37 @@
+package main
+
+type LinkNode struct {
+	data interface{}
+	next *LinkNode
+}
+
+type Link struct {
+	head *LinkNode
+	tail *LinkNode
+}
+
+func (p *Link)InsertHead(data interface{})  {
+	node := &LinkNode{
+		data:data,
+		next:nil,
+	}
+	node.next = p.head
+	p.head = node
+}
+
+func (p *Link)InsertTail(data interface{})  {
+	node := &LinkNode{
+		data:data,
+		next:nil,
+	}
+	if p.tail == nil && p.head == nil{
+		p.tail  =node
+		p.head  =node
+		return
+	}
+	p.tail.next = node
+	p.tail = node
+}
+
+func main() {
+}
