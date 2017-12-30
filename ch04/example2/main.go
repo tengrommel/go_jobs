32d71e6b72c6f88301eb46f359e4798134b5b915
tengrommel <wwwsousoucom@126.com>
2017-12-30 11:26:36 +0800

    捕获异常

--- ch04/example2/main.go
+++ ch04/example2/main.go
@@ -2,7 +2,20 @@ package main
 
 import "fmt"
 
+func test()  {
+	defer func() {
+		if err := recover(); err != nil{
+			fmt.Println(err)
+		}
+	}()
+	b := 0
+	a := 100/b
+	fmt.Println(a)
+	return
+}
+
 func main() {
+	test()
 	var a []int
 	a = append(a,10,20,383)
 	a = append(a,a...)
