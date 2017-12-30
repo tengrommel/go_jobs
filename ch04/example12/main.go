0b5f2987a6d7daf217c54bffe28f1e0811cdde86
tengrommel <wwwsousoucom@126.com>
2017-12-30 15:36:16 +0800

    map翻转

--- ch04/example12/main.go
+++ ch04/example12/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func testMapSort()  {
 	var a map[int]int
@@ -10,11 +13,34 @@ func testMapSort()  {
 	a[4]=0
 	a[1]=3
 
-	for k, v := range a{
-		fmt.Println(k, v)
+	var keys []int
+	for k := range a{
+		keys = append(keys, k)
 	}
+	sort.Ints(keys)
+
+	for _, v :=range keys{
+		fmt.Println(v, a[v])
+	}
+}
+
+func revalsMap()  {
+	var a map[int]string
+	var b map[string]int
+	a = make(map[int]string)
+	b = make(map[string]int)
+
+	a[123] = "123"
+	a[234] = "234"
+
+	for k, v := range a {
+		b[v]=k
+	}
+
+	fmt.Println(b)
 }
 
 func main() {
 	testMapSort()
+	revalsMap()
 }
