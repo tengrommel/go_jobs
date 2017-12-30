7f5628a42221b8e88353ce2073bb2e9a7af460c9
tengrommel <wwwsousoucom@126.com>
2017-12-30 13:13:09 +0800

    range 和 for 两种方式

--- ch04/example6/main.go
+++ ch04/example6/main.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	var a [10]int
-	j := 10
+	j := 9
 	a[0] = 100
 	a[j] = 200
 	fmt.Println(a)
+
+	for i:=0;i<len(a);i++{
+		fmt.Println(a[i])
+	}
+
+	for index, item := range a{
+		fmt.Printf("a[%d]=%d\n", index, item)
+	}
 }
