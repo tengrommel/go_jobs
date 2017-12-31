8a2dc8401ef474e3899c5409a5deccb3f9daec96
tengrommel <wwwsousoucom@126.com>
2017-12-31 18:11:34 +0800

    login

--- ch10/example1/main.go
+++ ch10/example1/main.go
@@ -9,11 +9,16 @@ func Hello(w http.ResponseWriter, r *http.Request)  {
 	fmt.Println("handle hello")
 	fmt.Fprintf(w, "hello")
 }
+func login(w http.ResponseWriter, r *http.Request)  {
+	fmt.Println("handle login")
+	fmt.Fprintf(w, "login")
+}
 
 func main() {
 	http.HandleFunc("/", Hello)
+ 	http.HandleFunc("/user/login", login)
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err!= nil{
-		fmt.Println("err", err)
+		fmt.Println("err:", err)
 	}
 }
