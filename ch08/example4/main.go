60a22bfde421c08ec3c248b7e73b902bb9d833d1
tengrommel <wwwsousoucom@126.com>
2017-12-31 14:54:05 +0800

    select

---
+++ ch08/example4/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var ch chan int
+	ch = make(chan int, 10)
+	ch2 := make(chan int, 10)
+
+	for i:=0;i<10;i++ {
+		ch <- i
+		ch2 <- i*2
+	}
+	for  {
+		select {
+		case v := <- ch:
+			fmt.Println(v)
+		case v:= <-ch2:
+			fmt.Println(v)
+		default:
+			fmt.Println("get data timeout!")
+			time.Sleep(time.Second)
+		}
+	}
+
+}
