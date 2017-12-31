72669a25ce18a20a49da969cf80104482e85525e
tengrommel <wwwsousoucom@126.com>
2017-12-31 14:41:25 +0800

    注意关闭

---
+++ ch08/example3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func send (ch chan int, lock chan bool)  {
+	for i:=0;i<10;i++ {
+		ch <- i
+	}
+	close(ch)
+	lock <- true
+}
+
+func recv (ch chan int, lock chan bool)  {
+	for  {
+		v, ok := <- ch
+		if !ok {
+			break
+		}
+		fmt.Println(v)
+	}
+	lock<- true
+}
+func main() {
+	ch := make(chan int, 10)
+	lock := make(chan bool, 2)
+	go send(ch, lock)
+	go recv(ch, lock)
+
+	var total = 0
+	for _ = range lock{
+		total++
+		if total == 2{
+			break
+		}
+	}
+}
