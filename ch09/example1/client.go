72c9e6b4007b4817aee86b37da78bed0a0bfcebf
tengrommel <wwwsousoucom@126.com>
2017-12-31 15:30:27 +0800

    client

---
+++ ch09/example1/client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:5000")
+	if err != nil{
+		fmt.Println("Error dialing", err.Error())
+		return
+	}
+	defer conn.Close()
+	inputReader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := inputReader.ReadString('\n')
+		trimmedInput := strings.Trim(input, "\r\n")
+		if trimmedInput == "Q"{
+			return
+		}
+		_, err = conn.Write([]byte(trimmedInput))
+		if err != nil{
+			return
+		}
+	}
+
+}
