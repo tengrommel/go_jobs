3743855aaeb44fc55d933f2f1df778dc6e8fb346
tengrommel <wwwsousoucom@126.com>
2017-12-31 16:31:34 +0800

    redis_chat

---
+++ ch09/chat/proto/proto.go
@@ -0,0 +1,22 @@
+package proto
+
+import "github.com/tengrommel/go_jobs/ch09/chat/model"
+
+type Message struct {
+	Cmd  string `json:"cmd"`
+	Data string `json:"data"`
+}
+
+type LoginCmd struct {
+	Id     int    `json:"user_id"`
+	Passwd string `json:"passwd"`
+}
+
+type RegisterCmd struct {
+	User model.User `json:"user"`
+}
+
+type LoginCmdRes struct {
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
