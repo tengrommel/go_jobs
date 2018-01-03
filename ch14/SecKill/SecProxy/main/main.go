d8ed6ccdc927be3427ed1d88be34b6c17050b4a9
tengrommel <wwwsousoucom@126.com>
2018-01-03 21:18:52 +0800

    加入

--- ch14/SecKill/SecProxy/main/main.go
+++ ch14/SecKill/SecProxy/main/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "github.com/astaxie/beego"
-
+import (
+	"github.com/astaxie/beego"
+	_ "github.com/tengrommel/go_jobs/ch14/SecKill/SecProxy/router"
+)
 func main()  {
 	beego.Run()
 }
--- ch14/SecKill/SecProxy/router/router.go
+++ ch14/SecKill/SecProxy/router/router.go
@@ -2,9 +2,11 @@ package router
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 	"github.com/tengrommel/go_jobs/ch14/SecKill/SecProxy/controller"
 )
 func init()  {
+	logs.Debug("enter router init")
 	beego.Router("/seckill", &controller.SkillController{}, "*:SecKill")
 	beego.Router("/secinfo", &controller.SkillController{}, "*:SecInfo")
 }
