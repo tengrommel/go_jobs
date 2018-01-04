a11e777115578c4f88446296a6b8ff1a9112628a
tengrommel <wwwsousoucom@126.com>
2018-01-04 22:46:10 +0800

    initEtcd

--- ch14/SecKill/SecProxy/main/init.go
+++ ch14/SecKill/SecProxy/main/init.go
@@ -1,85 +1,205 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	"strings"
-
-	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/logs"
 	"github.com/tengrommel/go_jobs/ch14/SecKill/SecProxy/service"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/astaxie/beego/logs"
+	etcd_client "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
+	"github.com/garyburd/redigo/redis"
 )
 
 var (
-	secKillConf = &service.SecSkillConf{
-		SecProductInfoMap: make(map[int]*service.SecProductInfoConf, 1024),
-	}
+	redisPool  *redis.Pool
+	etcdClient *etcd_client.Client
 )
 
-func initConfig() (err error) {
-	redisAddr := beego.AppConfig.String("redis_addr")
-	etcdAddr := beego.AppConfig.String("etcd_addr")
-
-	logs.Debug("read config succ, redis addr:%v", redisAddr)
-	logs.Debug("read config succ, etcd addr:%v", etcdAddr)
-
-	secKillConf.EtcdConf.EtcdAddr = etcdAddr
-	secKillConf.RedisConf.RedisAddr = redisAddr
-
-	if len(redisAddr) == 0 || len(etcdAddr) == 0 {
-		err = fmt.Errorf("init config failed, redis[%s] or etcd[%s] config is null", redisAddr, etcdAddr)
-		return
+func initRedis() (err error) {
+	redisPool = &redis.Pool{
+		MaxIdle:     secKillConf.RedisConf.RedisMaxIdle,
+		MaxActive:   secKillConf.RedisConf.RedisMaxActive,
+		IdleTimeout: time.Duration(secKillConf.RedisConf.RedisIdleTimeout) * time.Second,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", secKillConf.RedisConf.RedisAddr)
+		},
 	}
 
-	redisMaxIdle, err := beego.AppConfig.Int("redis_max_idle")
+	conn := redisPool.Get()
+	defer conn.Close()
+
+	_, err = conn.Do("ping")
 	if err != nil {
-		err = fmt.Errorf("init config failed, read redis_max_idle error:%v", err)
+		logs.Error("ping redis failed, err:%v", err)
 		return
 	}
-
-	redisMaxActive, err := beego.AppConfig.Int("redis_max_active")
-	if err != nil {
-		err = fmt.Errorf("init config failed, read redis_max_active error:%v", err)
-		return
-	}
-
-	redisIdleTimeout, err := beego.AppConfig.Int("redis_idle_timeout")
-	if err != nil {
-		err = fmt.Errorf("init config failed, read redis_idle_timeout error:%v", err)
-		return
-	}
-
-	secKillConf.RedisConf.RedisMaxIdle = redisMaxIdle
-	secKillConf.RedisConf.RedisMaxActive = redisMaxActive
-	secKillConf.RedisConf.RedisIdleTimeout = redisIdleTimeout
-
-	etcdTimeout, err := beego.AppConfig.Int("etcd_timeout")
-	if err != nil {
-		err = fmt.Errorf("init config failed, read etcd_timeout error:%v", err)
-		return
-	}
-
-	secKillConf.EtcdConf.Timeout = etcdTimeout
-	secKillConf.EtcdConf.EtcdSecKeyPrefix = beego.AppConfig.String("etcd_sec_key_prefix")
-	if len(secKillConf.EtcdConf.EtcdSecKeyPrefix) == 0 {
-		err = fmt.Errorf("init config failed, read etcd_sec_key error:%v", err)
-		return
-	}
-
-	productKey := beego.AppConfig.String("etcd_product_key")
-	if len(productKey) == 0 {
-		err = fmt.Errorf("init config failed, read etcd_product_key error:%v", err)
-		return
-	}
-
-	if strings.HasSuffix(secKillConf.EtcdConf.EtcdSecKeyPrefix, "/") == false {
-		secKillConf.EtcdConf.EtcdSecKeyPrefix = secKillConf.EtcdConf.EtcdSecKeyPrefix + "/"
-	}
-
-	secKillConf.EtcdConf.EtcdSecProductKey = fmt.Sprintf("%s%s", secKillConf.EtcdConf.EtcdSecKeyPrefix, productKey)
-	secKillConf.LogPath = beego.AppConfig.String("log_path")
-	secKillConf.LogLevel = beego.AppConfig.String("log_level")
 
 	return
 }
 
+func initEtcd() (err error) {
+	cli, err := etcd_client.New(etcd_client.Config{
+		Endpoints:   []string{secKillConf.EtcdConf.EtcdAddr},
+		DialTimeout: time.Duration(secKillConf.EtcdConf.Timeout) * time.Second,
+	})
+	if err != nil {
+		logs.Error("connect etcd failed, err:", err)
+		return
+	}
+
+	etcdClient = cli
+	return
+}
+
+func convertLogLevel(level string) int {
+
+	switch level {
+	case "debug":
+		return logs.LevelDebug
+	case "warn":
+		return logs.LevelWarn
+	case "info":
+		return logs.LevelInfo
+	case "trace":
+		return logs.LevelTrace
+	}
+
+	return logs.LevelDebug
+}
+
+func initLogger() (err error) {
+	config := make(map[string]interface{})
+	config["filename"] = secKillConf.LogPath
+	config["level"] = convertLogLevel(secKillConf.LogLevel)
+
+	configStr, err := json.Marshal(config)
+	if err != nil {
+		fmt.Println("marshal failed, err:", err)
+		return
+	}
+
+	logs.SetLogger(logs.AdapterFile, string(configStr))
+	return
+}
+
+func loadSecConf() (err error) {
+
+	resp, err := etcdClient.Get(context.Background(), secKillConf.EtcdConf.EtcdSecProductKey)
+	if err != nil {
+		logs.Error("get [%s] from etcd failed, err:%v", secKillConf.EtcdConf.EtcdSecProductKey, err)
+		return
+	}
+
+	var secProductInfo []service.SecProductInfoConf
+	for k, v := range resp.Kvs {
+		logs.Debug("key[%v] valud[%v]", k, v)
+		err = json.Unmarshal(v.Value, &secProductInfo)
+		if err != nil {
+			logs.Error("Unmarshal sec product info failed, err:%v", err)
+			return
+		}
+
+		logs.Debug("sec info conf is [%v]", secProductInfo)
+	}
+
+	updateSecProductInfo(secProductInfo)
+	return
+}
+
+func initSec() (err error) {
+
+	err = initLogger()
+	if err != nil {
+		logs.Error("init logger failed, err:%v", err)
+		return
+	}
+	/*
+		err = initRedis()
+		if err != nil {
+			logs.Error("init redis failed, err:%v", err)
+			return
+		}
+	*/
+	err = initEtcd()
+	if err != nil {
+		logs.Error("init etcd failed, err:%v", err)
+		return
+	}
+
+	err = loadSecConf()
+	if err != nil {
+		logs.Error("load sec conf failed, err:%v", err)
+		return
+	}
+
+	service.InitService(secKillConf)
+	initSecProductWatcher()
+
+	logs.Info("init sec succ")
+	return
+}
+
+func initSecProductWatcher() {
+	go watchSecProductKey(secKillConf.EtcdConf.EtcdSecProductKey)
+}
+
+func watchSecProductKey(key string) {
+
+	cli, err := etcd_client.New(etcd_client.Config{
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		logs.Error("connect etcd failed, err:", err)
+		return
+	}
+
+	logs.Debug("begin watch key:%s", key)
+	for {
+		rch := cli.Watch(context.Background(), key)
+		var secProductInfo []service.SecProductInfoConf
+		var getConfSucc = true
+
+		for wresp := range rch {
+			for _, ev := range wresp.Events {
+				if ev.Type == mvccpb.DELETE {
+					logs.Warn("key[%s] 's config deleted", key)
+					continue
+				}
+
+				if ev.Type == mvccpb.PUT && string(ev.Kv.Key) == key {
+					err = json.Unmarshal(ev.Kv.Value, &secProductInfo)
+					if err != nil {
+						logs.Error("key [%s], Unmarshal[%s], err:%v ", err)
+						getConfSucc = false
+						continue
+					}
+				}
+				logs.Debug("get config from etcd, %s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
+			}
+
+			if getConfSucc {
+				logs.Debug("get config from etcd succ, %v", secProductInfo)
+				updateSecProductInfo(secProductInfo)
+			}
+		}
+
+	}
+}
+
+func updateSecProductInfo(secProductInfo []service.SecProductInfoConf) {
+
+	var tmp map[int]*service.SecProductInfoConf = make(map[int]*service.SecProductInfoConf, 1024)
+	for _, v := range secProductInfo {
+		tmp[v.ProductId] = &v
+	}
+
+	secKillConf.RWSecProductLock.Lock()
+	secKillConf.SecProductInfoMap = tmp
+	secKillConf.RWSecProductLock.Unlock()
+}
